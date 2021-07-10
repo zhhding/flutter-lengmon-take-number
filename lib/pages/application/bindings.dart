e775d830f758b92f1248e6fc79fb810b093d20b0
zhhding <391320750@qq.com>
2021-07-10 14:16:52 +0800

    Initial commit

---
+++ lib/pages/application/bindings.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_ducafecat_news_getx/pages/main/index.dart';
+import 'package:get/get.dart';
+
+import 'controller.dart';
+
+class ApplicationBinding implements Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<ApplicationController>(() => ApplicationController());
+    Get.lazyPut<MainController>(() => MainController());
+  }
+}
